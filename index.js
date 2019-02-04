304cc799bfe30f5921f35c24b41bd7e6be606880
Samuel Cornet <samuel@noukod.com>
2019-02-04 19:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Code your solution in this file.
- // const newDrivers = function lowerCaseDrivers(drivers, function(lcaseDrivers)){
- //  return drivers.map(function(driver)) { return driver.toLowerCase});
- //  return newDrivers;
+function lowerCaseDrivers(drivers, function(lcaseDrivers)){
+  return drivers.map(function(driver)) { return driver.toLowerCase});
+  // return newDrivers;
 
-  // function map(drivers, function(lowCaseDrivers)) {
-      // const newArr = [];
-      //
-      // for (const drivers of drivers) {
-      //     newArr.push(callback(lowCaseDrivers));
-      // }
+  function map(drivers, function(lowCaseDrivers)) {
+      const newArr = [];
 
-  //     return newArr;
-  // }
+      for (const drivers of drivers) {
+          newArr.push(callback(lowCaseDrivers));
+      }
+
+      return newArr;
+  }
