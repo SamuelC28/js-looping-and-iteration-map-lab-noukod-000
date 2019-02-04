6d6ebf4492473e6b059b05e3917691083b240060
Samuel Cornet <samuel@noukod.com>
2019-02-04 19:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
  const newDrivers = function lowerCaseDrivers(drivers, function(lcaseDrivers)){
   return drivers.map(function(driver)) { return driver.toLowerCase});
   return newDrivers;
-  
+
   function map(drivers, function(lowCaseDrivers)) {
       const newArr = [];
-   
+
       for (const drivers of drivers) {
           newArr.push(callback(lowCaseDrivers));
       }
-   
+
       return newArr;
   }
