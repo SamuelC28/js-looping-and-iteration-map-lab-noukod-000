35b15811f6d6e055e4d3da8249203344d63cb9c0
Samuel Cornet <samuel@noukod.com>
2019-02-04 19:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file.
  function lowerCaseDrivers(drivers){
-  return drivers.map(function(driver).toLowerCase
+  return drivers.map(function(driver).toLowerCase)
   //  { return driver.toLowerCase});
 }
