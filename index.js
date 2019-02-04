b58bb1a02f8c4a02e2ac5631ee5eaed75ded2a5f
Samuel Cornet <samuel@noukod.com>
2019-02-04 19:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function lowerCaseDrivers(drivers, function(lcaseDrivers)){
   return drivers.map(function(driver)) { return driver.toLowerCase});
   // return newDrivers;
 
-  function map(drivers, function(lowCaseDrivers)) {
-      const newArr = [];
-
-      for (const drivers of drivers) {
-          newArr.push(callback(lowCaseDrivers));
-      }
-
-      return newArr;
-  }
+  // function map(drivers, function(lowCaseDrivers)) {
+  //     const newArr = [];
+  // 
+  //     for (const drivers of drivers) {
+  //         newArr.push(callback(lowCaseDrivers));
+  //     }
+  // 
+  //     return newArr;
+  // }
